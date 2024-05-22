b2468b8258dfd95882720dc9a1cb282969d9ba41
marious93 <marious_93@mail.ru>
2024-05-22 23:05:13 +1000

    Comment

--- src/com/company/MySecondApp.java
+++ src/com/company/MySecondApp.java
@@ -4,6 +4,7 @@ package com.company;
         public static void main(String[] args) {
             for (String str : args) {
                 System.out.println("аргумент = " + str);
+                //hello World
             }
         }
     }
